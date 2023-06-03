2734ca9d4117f012603faee1ace410ee7ca788c2
MyselfRoshan <roshanghimire699@gmail.com>
2023-06-03 21:32:00 +0545

    feat:added temp change function for weather app

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,6 @@ import Forecast from "./components/Forecast";
 import SearchAndLocation from "./components/search/SearchAndLocation";
 
 function App() {
-  // console.log(navigator.geolocation.getCurrentPosition());
-  // navigator.geolocation.getCurrentPosition((position) => {
-  //   let lat = position.coords.latitude;
-  //   let long = position.coords.longitude;
-  //   console.log(lat, long);
-  //   latText.innerText = lat.toFixed(2);
-  //   longText.innerText = long.toFixed(2);
-  // });
   return (
     <div className="mx-auto max-w-screen-xl mt-10 py-6 px-32 bg-gradient-to-br from-cyan-700 to-blue-800 h-fit shadow-xl shadow-gray-600">
       <TopCities />
